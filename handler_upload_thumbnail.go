6e108ee5cfa1b18964a8f519d2982f1d6694c6a2
andreas.oentoro <oentoro.andreas@gmail.com>
2026-02-22 11:52:05 +0800

    Show thumbnail upload logic and

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
+	// I just bit-shifted the number 10 to the left 20 times to get an int that stores the proper number of bytes.
+	// Bit shifting is a way to multiply by powers of 2. 10 << 20 is the same as 10 * 1024 * 1024, which is 10MB.
+	// Use r.FormFile to get the file data and file headers. The key the web browser is using is called "thumbnail"
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	imageBytes, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read image bytes", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video metadata", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You can't upload a thumbnail for this video", err)
+		return
+	}
+
+	thumbnailData := thumbnail{
+		data:      imageBytes,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = thumbnailData
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	videoMetadata.ThumbnailURL = &thumbnailUrl
+
+	err = cfg.db.UpdateVideo(videoMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
